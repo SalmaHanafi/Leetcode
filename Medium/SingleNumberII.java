bc10384745c4b1a462331945377d98592bf2efda
Salma Hanafi <shanafi2017@gmail.com>
2021-02-20 17:59:19 +0200

    Create SingleNumberII.java

---
+++ Medium/SingleNumberII.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+            
+        int answer = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Integer count; 
+        for(int i = 0; i < nums.length; i++){
+            
+            if(map.containsKey(nums[i])){
+                count = map.get(nums[i]);
+                map.put(nums[i], count+1);
+            }
+            else{
+                
+            map.put(nums[i], 1);
+            }
+        }
+        for( Integer key : map.keySet()){
+            if(map.get(key) == 1){
+                answer = key;
+            }
+            
+        }
+        return answer;
+    }
+    
+    
+}
