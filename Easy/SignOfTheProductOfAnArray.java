1fe1162ad78582edb25105e5d1c9e497b0931f54
Salma Hanafi <shanafi2017@gmail.com>
2021-05-01 12:57:18 +0200

    Create SignOfTheProductOfAnArray.java

---
+++ Easy/SignOfTheProductOfAnArray.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/sign-of-the-product-of-an-array/
+class Solution {
+    public int arraySign(int[] nums) {
+        int sign = 1;
+        for(int i : nums){
+            if(i==0) return 0;
+            if(i < 0) sign *= -1;
+        }
+            return sign;
+    }
+}
